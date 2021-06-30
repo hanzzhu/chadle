ac1d63b0410bb43f3fab362bb00abfc2e8790b9d
Han <hzhu009@e.ntu.edu.sg>
2021-06-30 09:19:15 +0800

    Update

--- leetcode.py
+++ leetcode.py
@@ -1,20 +1,34 @@
-def isPalindrome(x: int) -> bool:
+def longestPalindrome(s: str) -> str:
+    result = ''
+    list1 = list(s)
+    left = 0
+    right = 0
+    for left in range(len(list1)):
+        char = list1[left]
+        for right in range(len(list1)):
+            if right > left and char == list1[right]:
+                string = ''.join(list1[left:right+1])
+                # print(string)
+                if isPalindrome(string) and len(string) > len(result):
+                    result = string
+            right += 1
+        left += 1
 
-    list1 = list(str(x))
-    n = len(str(x))
-    if x == 0:
+    print(result)
+    return result
+
+
+def isPalindrome(s: str) -> bool:
+    stringList = list(s)
+    tempList = []
+    while stringList:
+
+        tempList.append(stringList.pop())
+
+    if tempList == list(s):
         return True
-    for i in range(len(list1)):
-        while n > i:
-            if list1[i] == list1[n-1]:
-                i += 1
-                n -= 1
-                if i == n-1 or i == n-2:
-                    return True
-                else:
-                    return False
-            else:
-                return False
+    else:
+        return False
 
-
-print(isPalindrome(11))
+# print(isPalindrome('abcba'))
+longestPalindrome('abcbadifidab')
